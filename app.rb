0c2e42d107437bffc4a97b096dad64141b23e85f
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-11 15:37:22 +0100

    No, I did it MYYYY way

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "No, I did it MY way"
+end
+
+get '/kick' do
+  "ain't that a kick in the head"
+end
+
+get '/new' do
+  "New York, New York, New Yooooooooooorrrrrrrkkkkkkkkkkkkkk"
+end
+
+get '/fly' do
+  "Fly me to the moon, let me play among the stars, let me see what life is like on Jupiter and Mars"
+end
+
+get '/lady' do
+  "and thats why, the lady is a tramp"
+end
+
+get '/strangers' do
+  "Starngers in the night, exchanging glances......."
+end
+
+get '/cat' do
+  "<div style='border: 10px solid blue'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
